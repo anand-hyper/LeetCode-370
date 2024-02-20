452a85ce22ff4906aeb71dd4cb1c57622470ac1b
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-02-20 11:14:36 +0530

    Time: 3 ms (93.85%), Space: 10.7 MB (5.03%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,18 +2,8 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        long oldnum =x,rnum;
-        long lastdigit=0;
-        while(x>0)
-        {
-              lastdigit=x%10;
-              rnum=(rnum*10)+lastdigit;
-              x=x/10;
-        
-        
-        }
-        
-        return(oldnum==rnum);
-        
+        string rev = to_string(x);
+        reverse(rev.begin(),rev.end());
+        return(to_string(x)==rev);
     }
 };
