8bc2502f7db8f82b1d0c5a5e6342d8dfc590e291
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-04-28 11:07:20 +0530

    Time: 11 ms (91.15%), Space: 21.6 MB (73.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         
-        
-    vector<int> arr1;
-    int n=nums.size();
+    int j,n=nums.size();
     for(int i=0;i<n;i++)
     {
-      if (nums[i] != 0) {
-        arr1.push_back(nums[i]);
-      }
+       if(nums[i] ==0)
+       {
+           j=i;
+           break;
+           
+       }
     }
-    int k=arr1.size();
-    for(int i=0;i<k;i++)
+    
+    for(int i=j+1;i<n;i++)
     {
-        nums[i]=arr1[i];
+       if(nums[i]!=0)
+       {
+           swap(nums[i],nums[j]);
+           j++;
+           
+       }
 
     }
-    for(int j=k;j<n;j++)
-    {
-        nums[j]=0;
-    }
-     
-        
         
     }
 };
