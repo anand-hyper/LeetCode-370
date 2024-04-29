6687178f0aa20ccb2ecae22c8b0f89de675d3511
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-04-29 10:19:44 +0530

    Time: 12 ms (61.14%), Space: 13.7 MB (40.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-            int i,j;
-            vector<int> index;
-            for(i=0;i<nums.size();i++)
+            int left,right,sum;
+            vector<std::pair<int,int>> num_with_index;
+            for(int i=0;i<nums.size();i++)
             {
-               for(j=i+1;j< nums.size();j++)
-               {
-                     if(nums[i]+nums[j]==target)
-                     {
-                         index.push_back(i);
-                         index.push_back(j);
-                         return index;
-                         
-                     }
-                   
-               }
-
-
+                num_with_index.push_back({nums[i],i});
+                
+            }
+            sort(num_with_index.begin(),num_with_index.end());
+            
+            left =0,right=num_with_index.size()-1;
+            while(left<right)
+            {
+                 sum = num_with_index[left].first + num_with_index[right].first;
+                 if(sum==target)
+                 {
+                     
+                     return {num_with_index[left].second,num_with_index[right].second};
+                 }
+                
+                 else if(sum>target) right--;
+                 else left++;
+                
+                
             }
             
-            return index;
+
+            
+            
+            return {};
         
             
         
