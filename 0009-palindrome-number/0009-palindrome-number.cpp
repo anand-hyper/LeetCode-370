5193d662e4045c105fd4a9f84d5717626deb9e00
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-05-09 19:09:48 +0530

    Time: 8 ms (60.43%), Space: 8.3 MB (48.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,8 +2,31 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         
-        string rev = to_string(x);
-        reverse(rev.begin(),rev.end());
-        return(to_string(x)==rev);
+          int num =x,reversenum=0,rem=0;
+          if(num<0)
+          {
+              return false;
+              
+          }
+          
+          while(num!=0)
+          {
+              rem=num%10;
+              if(reversenum>INT_MAX/10 || reversenum==INT_MAX/10 && rem>7)
+              {
+                  return false;
+              }
+              reversenum=reversenum*10+rem;
+              num=num/10;
+          }
+          if(reversenum==x)
+          {
+              return true;
+              
+          }
+          else 
+          {
+              return false;
+          }
     }
 };
