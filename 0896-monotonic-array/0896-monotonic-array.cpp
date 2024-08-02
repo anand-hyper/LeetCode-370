edb9f7c8e00fe7d11cccc1da911b86fc0a21e2fa
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-08-02 19:42:04 +0530

    Time: 112 ms (61.47%), Space: 99.3 MB (12.38%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -3,17 +3,17 @@ public:
     bool isMonotonic(vector<int>& nums) {
         int inc = true,dec = true;
         int n = nums.size();
+        if((nums[n-1] - nums[0]) < 0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
         for(int i=0;i<n-1;i++)
         {
-            if(!(nums[i] <= nums[i+1]))
+            if(nums[i]>nums[i+1])
             {
-                inc = false;
-            }
-            else if(!(nums[i] >= nums[i+1]))
-            {
-                dec = false;
+                return false;
             }
         }
-        return inc || dec;
+        return true;
     }
 };
