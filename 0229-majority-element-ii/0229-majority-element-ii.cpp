8ba86e5f8529b44ab3b0f2f6e92ea73034a80bb4
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-09-03 10:42:41 +0530

    Time: 12 ms (29.33%), Space: 19.9 MB (7.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,18 +3,20 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
        int n = nums.size();
        unordered_map<int,int>mpp;
+       int min = int(n/3)+1;;
+       vector<int> ans;
        for(int i=0;i<n;i++)
        {
            mpp[nums[i]]++;
-       }
-       int val = n/3;
-       vector<int> ans;
-       for(auto each : mpp)
-       {
-           if(each.second>val)
-             ans.push_back(each.first);
+           if(mpp[nums[i]]==min)
+           {
+                ans.push_back(nums[i]);
+           }
+           if(ans.size()==2) break;
        }
        return ans;
+      
+      
        
     
     }
