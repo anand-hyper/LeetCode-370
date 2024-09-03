180332f3483b82c8cbd3068400754730e0bebf48
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-09-03 11:19:13 +0530

    Time: 10 ms (48.22%), Space: 18.7 MB (71.00%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,18 +2,42 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
        int n = nums.size();
-       unordered_map<int,int>mpp;
+     
        int min = int(n/3)+1;;
        vector<int> ans;
+       int el1=0,el2=0;
+       int cnt1=0,cnt2=0; 
+        //moores  voting algorithm
        for(int i=0;i<n;i++)
        {
-           mpp[nums[i]]++;
-           if(mpp[nums[i]]==min)
+         
+          
+          if(cnt1==0 && nums[i] != el2)
+          {
+              cnt1=1;
+              el1=nums[i];
+          }
+           else if(cnt2==0 && nums[i] != el1)
            {
-                ans.push_back(nums[i]);
+               cnt2=1;
+               el2=nums[i];
            }
-           if(ans.size()==2) break;
+           else if(el1==nums[i]) cnt1++;
+           else if(el2==nums[i]) cnt2++;
+           else 
+           {
+               cnt1--;
+               cnt2--;
+           }
        }
+       cnt1 =0,cnt2=0;
+       for(int i=0;i<n;i++)
+       {
+           if(nums[i]==el1) cnt1++;
+           else if(nums[i]==el2) cnt2++;
+       }
+       if(cnt1>=min) ans.push_back(el1);
+       if(cnt2>=min) ans.push_back(el2);
        return ans;
       
       
