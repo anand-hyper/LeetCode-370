81512a39201b274145baf1d21287486cd085346f
anand-hyper <117594948+anand-hyper@users.noreply.github.com>
2024-09-04 19:11:38 +0530

    Time: 7 ms (27.30%), Space: 11.4 MB (29.68%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,37 +4,19 @@ public:
         
         int n = strs.size();
         string ans = "";
-        if(n==1) return strs[0];
-        for(int i=0;i<n-1;i++)
+        sort(strs.begin(),strs.end());
+        
+        string first = strs[0],last = strs[n-1];
+        
+        for(int i=0;i<min(first.size(),last.size());i++)
         {
-            string temp = "";
-            string s1 = strs[i];
-            string s2 = strs[i+1];
-            int n1 = s1.length();
-            int n2 = s2.length();
-            int left = 0, right = 0;
-            while(left<n1 && right < n2)
+            if(first[i] != last[i])
             {
-                if(s1[left]==s2[right])
-                {
-                    temp += s1[left];
-                    left++;
-                    right++;
-                }
-                else
-                {
-                    break;
-                }
+                return ans;
             }
-            if(i==0)
-            {
-                ans = temp;
-            }
-            else if(temp.length() < ans.length())
-            {
-                ans = temp;
-            }
+            ans+=first[i];
         }
+        
         return ans;
     }
 };
